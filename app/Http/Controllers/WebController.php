6e1791c6e6d31fb73b891f5786a7c52eddbdca9a
saki30 <s6u6s024@gmail.com>
2025-09-01 15:36:03 +0900

    Fix featured_products orderBy to avoid error

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -15,10 +15,11 @@ class WebController extends Controller
         $major_categories = MajorCategory::all();
 
         $recently_products = Product::orderByDesc('created_at')->take(4)->get();
-        
+
         $recommend_products = Product::where('recommend_flag', true)->take(3)->get();
 
         $featured_products = Product::withAvg('reviews', 'score')
+            ->orderByDesc('reviews_avg_score')
             ->orderByDesc('created_at') 
             ->take(4)
             ->get();
