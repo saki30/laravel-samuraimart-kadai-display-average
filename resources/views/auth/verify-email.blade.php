0334b245ce75be341f96014cf508358e55a4a8ae
saki30 <s6u6s024@gmail.com>
2025-09-01 21:57:42 +0900

    Update verify-email page message

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -4,13 +4,20 @@
 <div class="container pt-5">
     <div class="row justify-content-center">
         <div class="col-md-5">
-            <h1 class="text-center mb-3">会員登録ありがとうございます！</h1>
-
-            <p class="text-center lh-lg mb-5">
-                現在、仮会員の状態です。<br>
-                ただいま、ご入力いただいたメールアドレス宛にご本人様確認用のメールをお送りしました。<br>
-                メール本文内のURLをクリックすると本会員登録が完了となります。
-            </p>
+            @if (session('register_message'))
+                <h1 class="text-center mb-3">会員登録ありがとうございます！</h1>
+                <p class="text-center lh-lg mb-5">
+                    現在、仮会員の状態です。<br>
+                    ただいま、ご入力いただいたメールアドレス宛にご本人様確認用のメールをお送りしました。<br>
+                    メール本文内のURLをクリックすると本会員登録が完了となります。
+                </p>
+            @else
+                <h1 class="text-center mb-3">メールアドレスの確認が必要です</h1>
+                <p class="text-center lh-lg mb-5">
+                    ご登録いただいたメールアドレスに確認メールを送信しました。<br>
+                    本会員登録を完了するには、メール内のリンクをクリックしてください。
+                </p>
+            @endif
 
             <div class="text-center">
                 <a href="{{ url('/') }}" class="btn samuraimart-submit-button w-75 text-white">トップページへ</a>
